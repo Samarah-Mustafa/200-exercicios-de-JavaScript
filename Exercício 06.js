59c3f148a6f3cd8d96700952fa4deb9b95f9cd79
Samarah Mustafá <samarah.dev@gmail.com>
2025-09-16 22:31:53 -0300

    True/ False com AND, OR e NOT

---
+++ Exercício 06.js
@@ -0,0 +1,14 @@
+//Exercício 6: Declare duas variáveis e atribua valores booleanos a elas.
+// Use os operadores lógicos AND, OR e NOT para realizar operações lógicas entre as variáveis.
+// Imprima os resultados.
+
+// Declarando as variáveis e atribuindo valores booleanos
+
+const x = true;
+const y = false;
+
+console.log("x && y:", x && y);
+
+console.log("x || y:", x || y);
+console.log("!x:", !x);
+console.log("!y:", !y);
