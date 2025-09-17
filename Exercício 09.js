4f9276da484f9028a5e5d739e52d97e564466ff2
Samarah Mustafá <samarah.dev@gmail.com>
2025-09-16 22:52:36 -0300

    Positivo/Negativo ou Zero

---
+++ Exercício 09.js
@@ -0,0 +1,11 @@
+//Exercício 9: Escreva um programa que recebe um número e verifica se ele é positivo, negativo ou zero.
+
+let numero = -2;
+
+if (numero > 0) {
+    console.log("O número é positivo.");
+} else if (numero < 0) {
+    console.log("O número é negativo.");
+} else {
+    console.log("O número é zero.");
+}
