4e87ded99280bc4304dfa8cc8d1833f0d8dddc11
Samarah Mustafá <samarah.dev@gmail.com>
2025-09-16 23:11:34 -0300

    Idade mínima pra votar

---
+++ Exercício 11.js
@@ -0,0 +1,9 @@
+//Exercício 11: Escreva um programa que verifica se uma pessoa pode votar com base na idade.
+
+let idade = 16;
+
+if (idade >= 18) {
+    console.log("Autorizado(a) a votar.");
+} else {
+    console.log("Não autorizado(a) a votar.");
+}
