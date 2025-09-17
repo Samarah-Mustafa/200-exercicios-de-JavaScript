e4882dc394589a2ef98d11b2772d902588194ae4
Samarah Mustafá <samarah.dev@gmail.com>
2025-09-16 22:38:04 -0300

    True/False com operadores de comparação

---
+++ Exercício 07.js
@@ -0,0 +1,10 @@
+//Exercício 7: Declare três variáveis e atribua valores numéricos a elas. Use operadores de comparação para comparar os valores entre as variáveis. Imprima os resultados.
+
+let a = 2;
+let b = 25;
+let c = 48;
+
+console.log(a < b); 
+console.log(b < c);
+console.log(a === b);
+console.log(b !== c);
