4df2d847fc45b49e93a29e3f177e6f7d7328896a
Samarah Mustafá <samarah.dev@gmail.com>
2025-09-16 22:07:29 -0300

    Nome e Sobrenome

---
+++ Exercício 04.js
@@ -0,0 +1,8 @@
+//Exercício 4: Declare duas variáveis chamadas "nome" e "sobrenome", atribua o seu nome e sobrenome a elas.
+// Concatene-as em uma terceira variável chamada "nomeCompleto" e imprima.
+
+let nome = "Samarah";
+let sobrenome = "Mustafá";
+let nomeCompleto = nome + " " + sobrenome;
+
+console.log(nomeCompleto);
