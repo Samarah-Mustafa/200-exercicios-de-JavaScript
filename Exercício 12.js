411ddf793bc9fd1b6f4afbcc884ea0a65911d64b
Samarah Mustafá <samarah.dev@gmail.com>
2025-09-16 23:20:51 -0300

    Situação de um estudante

---
+++ Exercício 12.js
@@ -0,0 +1,11 @@
+//Exercício 12: Escreva um programa que verifica a situação de um estudante de acordo com sua média final.
+
+let mediaFinal = 6.5;
+
+if (mediaFinal >= 7) {
+    console.log("O estudante está aprovado.");
+} else if (mediaFinal >= 5) {
+    console.log("O estudante está em recuperação.");
+} else {
+    console.log("O estudante está reprovado.");
+}
