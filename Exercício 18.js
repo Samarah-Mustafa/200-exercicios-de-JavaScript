2df8a08935a8d2dcf15dc945adf15cb357b963ff
Samarah Mustafá <samarah.dev@gmail.com>
2025-09-18 22:50:02 -0300

    Idade mínima para dirigir

---
+++ Exercício 18.js
@@ -0,0 +1,8 @@
+//Exercício 18: Escreva um programa que verifica se uma pessoa pode dirigir com base na idade.
+const idade = 16;
+
+if (idade >= 18) {
+    console.log("Você pode dirigir.");
+} else {
+    console.log("Você não pode dirigir.");
+}
