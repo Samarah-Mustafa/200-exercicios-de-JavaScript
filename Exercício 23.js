c6426a91c581352144c2beefc4acbeb6c0a04bd9
Samarah Mustafá <samarah.dev@gmail.com>
2025-09-19 13:28:19 -0300

    Verificando as cores do semáforo

---
+++ Exercício 23.js
@@ -0,0 +1,12 @@
+//Exercício 23: Escreva um programa que verifica as cores de um semáforo.
+let cor = "verde";
+
+if (cor === "verde") {
+    console.log("Pode passar.");
+} else if (cor === "amarelo") {
+    console.log("Atenção! Prepare-se para parar.");
+} else if (cor === "vermelho") {
+    console.log("Pare!");
+} else {
+    console.log("Cor inválida.");
+}
