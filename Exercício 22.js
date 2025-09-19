0c6c009a0a691c9ff408f9881689d9507fc87413
Samarah Mustafá <samarah.dev@gmail.com>
2025-09-19 11:12:29 -0300

    Número de dias em um mês

---
+++ Exercício 22.js
@@ -0,0 +1,21 @@
+//Exercício 22: Escreva um programa que determina o número de dias em um mês, considerando ano bissexto.
+
+let mes = 2;
+let ano = 2024;
+let dias;
+
+function ehBissexto(ano) {
+    return (ano % 4 === 0 && ano % 100 !== 0) || (ano % 400 === 0);
+}
+
+if (mes === 2) {
+    dias = ehBissexto(ano) ? 29 : 28;
+} else if (mes === 4 || mes === 6 || mes === 9 || mes === 11) {
+    dias = 30;
+} else if (mes >= 1 && mes <= 12) {
+    dias = 31;
+} else {
+    dias = "Mês inválido";
+}
+
+console.log("O mês " + mes + " do ano " + ano + " tem " + dias + " dias.");
