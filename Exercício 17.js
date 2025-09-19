eff413e3c080939ee7887ff732240e2c57c5b08e
Samarah Mustafá <samarah.dev@gmail.com>
2025-09-18 22:37:14 -0300

    Par ou Ímpar

---
+++ Exercício 17.js
@@ -0,0 +1,8 @@
+//Exercício 17: Escreva um programa que determina se um número é par ou ímpar.
+let numero = 13;
+
+if (numero % 2 === 0) {
+    console.log(numero + " é par.");
+} else {
+    console.log(numero + " é ímpar.");
+}
