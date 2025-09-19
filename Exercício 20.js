124cae13c4b4165ad509c4e0d1f1c5706ecccc32
Samarah Mustafá <samarah.dev@gmail.com>
2025-09-18 23:11:37 -0300

    Classificação baseada na nota do aluno

---
+++ Exercício 20.js
@@ -0,0 +1,12 @@
+//Exercício 20: Escreva um programa que imprime uma classificação baseada na nota de um aluno.
+let nota = 8;
+
+if (nota >= 9) {
+    console.log("Classificação: Excelente");
+} else if (nota >= 7) {
+    console.log("Classificação: Bom");
+} else if (nota >= 5) {
+    console.log("Classificação: Regular");
+} else {
+    console.log("Classificação: Insuficiente");
+}
