1dc229970a7c8b1ed2c1dc3e311ca1d084a76a7e
Samarah Mustafá <samarah.dev@gmail.com>
2025-09-18 22:58:16 -0300

    Número entre um intervalo

---
+++ Exercício 19.js
@@ -0,0 +1,10 @@
+//Exercício 19: Escreva um programa que verifica se um número está dentro de um determinado intervalo.
+let numero = 18;
+let minimo = 10;
+let maximo = 25;
+
+if (numero >= minimo && numero <= maximo) {
+    console.log(numero + " está dentro do intervalo.");
+} else {
+    console.log(numero + " está fora do intervalo.");
+}
