f37b80ab27c88ef054342af2b3018c1af3f47973
Samarah Mustafá <samarah.dev@gmail.com>
2025-09-19 14:16:24 -0300

    imprimindo números ímpares de 1 a 20

---
+++ Exercício 26.js
@@ -0,0 +1,8 @@
+//Exercício 26: Usando um laço do-while, imprima todos os números ímpares de 1 a 20.
+let i = 1;
+do {
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+    i++;
+} while (i <= 20);
