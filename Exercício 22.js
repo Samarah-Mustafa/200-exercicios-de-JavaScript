178b5db67cbda48b7bb966edfa6fb6b0d5353970
Samarah Mustafá <samarah.dev@gmail.com>
2025-09-19 11:19:50 -0300

    Número de dias em um mês

--- Exercício 22.js
+++ Exercício 22.js
@@ -4,12 +4,12 @@ let mes = 2;
 let ano = 2024;
 let dias;
 
-function ehBissexto(ano) {
+function Bissexto(ano) {
     return (ano % 4 === 0 && ano % 100 !== 0) || (ano % 400 === 0);
 }
 
 if (mes === 2) {
-    dias = ehBissexto(ano) ? 29 : 28;
+    dias = Bissexto(ano) ? 29 : 28;
 } else if (mes === 4 || mes === 6 || mes === 9 || mes === 11) {
     dias = 30;
 } else if (mes >= 1 && mes <= 12) {
