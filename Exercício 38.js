feeacb4ab5b139626b32e90a4698a5d85ea37dbc
Samarah Mustafá <samarah.dev@gmail.com>
2025-09-21 15:15:21 -0300

    A soma dos cubos dos dez primeiros números naturais

---
+++ Exercício 38.js
@@ -0,0 +1,12 @@
+// Exercício 38: Usando um laço do-while, imprima a soma dos cubos dos primeiros 10 números naturais.
+let somaCubos = 0;
+let i = 1;
+
+do {
+  somaCubos += i * i * i;
+  i++;
+} while (i <= 10);
+
+console.log(somaCubos);
+console 
+.log(somaCubos);
