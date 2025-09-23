65176cbbbf0a745630819d38e8e8771e7c3d6159
Samarah Mustafá <samarah.dev@gmail.com>
2025-09-23 17:26:34 -0300

    Transformando número binário em decimal

---
+++ Exercício 42.js
@@ -0,0 +1,9 @@
+//Exercício 42: Usando um laço for, crie um programa que transforme um número binário em decimal.
+let numeroBinario = '111';
+let numeroDecimal = 0;
+let comprimento = numeroBinario.length;
+
+for (let i = 0; i < comprimento; i++) {
+  let digito = parseInt(numeroBinario[comprimento - 1 - i]);
+  numeroDecimal += digito * Math.pow(2, i);
+}
