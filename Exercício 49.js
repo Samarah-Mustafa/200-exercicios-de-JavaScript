587078fd86df6ce9875ee09105473effde7b65b4
Samarah Mustafá <samarah.dev@gmail.com>
2025-09-25 21:43:56 -0300

    Função que retorne o maior entre três números

---
+++ Exercício 49.js
@@ -0,0 +1,5 @@
+//Exercício 49: Escreva uma função que aceite três números como argumentos e retorne o maior deles.
+function maiorDeTres(a, b, c) {
+    return Math.max(a, b, c);
+}
+console.log(maiorDeTres(3, 7, 5));
