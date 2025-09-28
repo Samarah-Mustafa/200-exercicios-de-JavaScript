853f849f6f1162901a3b12d1868801ba3bf57f4f
Samarah Mustafá <samarah.dev@gmail.com>
2025-09-27 23:40:02 -0300

    A soma dos números após aplicação da função callback

---
+++ Exercício 65.js
@@ -0,0 +1,7 @@
+//Exercício 65: Escreva uma função que aceite um array de números e uma função de callback e retorne a soma de todos os números do array após a aplicação da função de callback.
+function somarComCallback(array, callback) {
+    return array.map(callback).reduce((a, b) => a + b, 0);
+}
+let array = [1, 2, 3, 4];
+let callback = num => num * 2;
+console.log(somarComCallback(array, callback));
