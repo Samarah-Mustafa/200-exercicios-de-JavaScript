1539372c6105ba56e1ce414524e2d4ed3bb3b9e7
Samarah Mustafá <samarah.dev@gmail.com>
2025-09-27 23:08:31 -0300

    Função que retorne um array com números primos

---
+++ Exercício 63.js
@@ -0,0 +1,20 @@
+//Exercício 63: Escreva uma função que receba um número e retorne um array com todos os números primos até aquele número.
+function numerosPrimos(ate) {
+    const primos = [];
+    for (let i = 2; i <= ate; i++) {
+        if (Primo(i)) {
+            primos.push(i);
+        }
+    }
+    return primos;
+}
+
+function Primo(num) {
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+console.log(numerosPrimos(15));
