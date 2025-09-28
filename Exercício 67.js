fe06a4d27a11ebc9af16c70c50bf25cf556002e6
Samarah Mustafá <samarah.dev@gmail.com>
2025-09-28 10:36:52 -0300

    Retornando um array com resultado da função de callback

---
+++ Exercício 67.js
@@ -0,0 +1,11 @@
+//Exercício 67: Escreva uma função que aceite uma função de callback e um array de arrays, e retorne um novo array que contém os resultados de aplicar a função de callback a cada array.
+function aplicarCallbackEmArrays(arrayDeArrays, callback) {
+    return arrayDeArrays.map(callback);
+}
+const arrays = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+const resultado = aplicarCallbackEmArrays(arrays, array => array.reduce((a, b) => a + b, 0));
+console.log("Resultado:", resultado);
