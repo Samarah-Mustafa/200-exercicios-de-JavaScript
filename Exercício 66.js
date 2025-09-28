de61f9f9522900432f371e193c552121ba8e05de
Samarah Mustafá <samarah.dev@gmail.com>
2025-09-28 10:29:52 -0300

    Retornando o que é verdadeiro com a função de callback

--- Exercício 66.js
+++ Exercício 66.js
@@ -1,7 +1,24 @@
 //Exercício 66: Escreva uma função que aceite uma função de callback que retorne verdadeiro ou falso e um array, e retorne um novo array que contém apenas os elementos para os quais a função de callback retornou verdadeiro.
 function filtrarArray(array, callback) {
-    return array.filter(callback);
+    const novoArray = [];
+
+    for (let i = 0; i < array.length; i++) {
+        const elemento = array[i];
+        
+        const deveIncluir = callback(elemento, i, array);
+
+        if (deveIncluir === true) {
+            novoArray.push(elemento);
+        }
+    }
+
+    return novoArray;
 }
-let array = [1, 2, 3, 4, 5];
-let callback = num => num % 2 === 0;
-console.log(filtrarArray(array, callback));
+const usuarios = [
+    { nome: "Ana", ativo: true },
+    { nome: "Beto", ativo: false },
+    { nome: "Carla", ativo: true }
+];
+
+const ativos = filtrarArray(usuarios, usuario => usuario.ativo === true);
+console.log("Usuários Ativos:", ativos);
