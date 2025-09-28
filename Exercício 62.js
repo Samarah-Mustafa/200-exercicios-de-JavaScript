b17b0b81fdb469af1f8031113c6637eddc17d2f7
Samarah Mustafá <samarah.dev@gmail.com>
2025-09-27 22:59:02 -0300

    Função que repete uma string em um número de vezes

---
+++ Exercício 62.js
@@ -0,0 +1,5 @@
+//Exercício 62: Escreva uma função que receba um número e retorne uma string repetida aquele número de vezes.
+function repetirString(num) {
+    return 'JavaScript '.repeat(num);
+}
+console.log(repetirString(3));
