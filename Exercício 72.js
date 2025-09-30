896e44c2fe5485b786e8231d31c018f586577528
Samarah Mustafá <samarah.dev@gmail.com>
2025-09-29 22:54:22 -0300

    Função de callback retornando o número de vezes

---
+++ Exercício 72.js
@@ -0,0 +1,13 @@
+//Exercício 72: Escreva uma função que aceite um número, um array de funções de callback e um valor inicial, e retorne o resultado final após a aplicação de todas as funções de callback no valor inicial, o número de vezes especificado.
+function aplicarCallbacks(numero, callback, valorInicial) {
+    let resultado = valorInicial;
+    for (let i = 0; i < numero; i++) {
+       callback.forEach(funcao => {
+           resultado = funcao(resultado);
+       });
+    }
+    return resultado;
+}       
+let callback = [num => num * 2, num => num + 1];
+let valorInicial = 1;
+console.log(aplicarCallbacks(5, callback, valorInicial));
