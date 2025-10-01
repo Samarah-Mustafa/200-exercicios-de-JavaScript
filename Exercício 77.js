a8aaaa32eacd6ee824517e8c22099bc4df3f8d0b
Samarah Mustafá <samarah.dev@gmail.com>
2025-10-01 16:25:03 -0300

    Objeto Livro com Propriedades e Método Descrição.

---
+++ Exercício 77.js
@@ -0,0 +1,18 @@
+//Exercício 77: Crie um objeto que represente um livro, com propriedades para o título, autor e número de páginas. Em seguida, adicione um método ao objeto que imprima uma descrição do livro.
+const livro = {
+    titulo: "A Arte da Guerra",
+    autor: "Sun Tzu",
+    paginas: 130,
+
+    descricao: function() {
+        console.log(`
+             Detalhes do Livro 
+            ---------------------
+            Título: ${this.titulo}
+            Autor: ${this.autor}
+            Páginas: ${this.paginas}
+            ---------------------
+        `);
+    }
+};
+livro.descricao();
