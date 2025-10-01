fa846280adca1fbf9337a78315011ed3e22695f3
Samarah Mustafá <samarah.dev@gmail.com>
2025-10-01 16:43:49 -0300

    Objeto Cachorro: Nome, Raça e Idade Humana.

--- Exercício 78.js
+++ Exercício 78.js
@@ -1,7 +1,7 @@
 //Exercício 78: Crie um objeto "cachorro" com propriedades para raça, nome e idade. Adicione um método que retorne a idade do cachorro em anos humanos (idade do cachorro * 7).
 const cachorro = {
-    nome: "Rex",
-    raca: "Labrador",
+    nome: "Thor",
+    raca: "Pastor Alemão",
     idade: 5,
 
     idadeHumanaDescricao: function() {
