866d86e554ff527abf56639aac305ca757e6557f
Samarah Mustafá <samarah.dev@gmail.com>
2025-10-02 20:16:18 -0300

    Objeto círculo: raio, área, circunferência

---
+++ Exercício 82.js
@@ -0,0 +1,12 @@
+// Exercício 82: Crie um objeto "circulo" que possui uma propriedade de raio e dois métodos que calculam a área e a circunferência.
+const circulo = {
+    raio: 5,
+    calcularArea: function() {
+        return Math.PI * this.raio * this.raio;
+    },
+    calcularCircunferencia: function() {
+        return 2 * Math.PI * this.raio;
+    }
+};
+console.log(`Área do círculo: ${circulo.calcularArea().toFixed(2)}`);
+console.log(`Circunferência do círculo: ${circulo.calcularCircunferencia().toFixed(2)}`);
