a9308111a2170a2ebd0b665803c9eb60b046190a
Samarah Mustafá <samarah.dev@gmail.com>
2025-10-03 17:35:46 -0300

    Objeto retângulo: altura, largura e área

---
+++ Exercício 84.js
@@ -0,0 +1,15 @@
+// Exercício 84:  Crie um objeto "retangulo" que possui propriedades para altura e largura, e um método para calcular a área.
+const retangulo = {
+    altura: 8,
+    largura: 15,
+
+    calcularArea: function() {
+        return this.altura * this.largura;
+    }
+};
+
+const area = retangulo.calcularArea();
+
+console.log(`Altura: ${retangulo.altura}`);
+console.log(`Largura: ${retangulo.largura}`);
+console.log(`Área calculada: ${area}`);
