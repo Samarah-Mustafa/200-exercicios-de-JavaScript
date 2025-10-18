4933968a664a6c040bd0b81094ae44af3d2ab79a
Samarah Mustafá <samarah.dev@gmail.com>
2025-10-18 13:34:52 -0300

    Retornando a palavra mais longa de um array

---
+++ Exercício 89.js
@@ -0,0 +1,19 @@
+//Exercício 89: Dada uma string com várias palavras separadas por espaços, transforme-a em um array e depois retorne a palavra mais longa.
+
+function encontrarPalavraMaisLonga(str) {
+    const palavras = str.split(' ');
+    let palavraMaisLonga = '';
+    let tamanhoMaximo = 0;
+
+    for (let i = 0; i < palavras.length; i++) {
+        if (palavras[i].length > tamanhoMaximo) {
+            tamanhoMaximo = palavras[i].length;
+            palavraMaisLonga = palavras[i];
+        }
+    }
+
+    return palavraMaisLonga;
+}
+const frase = "Encontre a palavra mais longa nesta frase de exemplo";
+const resultado = encontrarPalavraMaisLonga(frase);
+console.log(`A palavra mais longa é: ${resultado}`);
