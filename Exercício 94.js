fbde5a5a51b2945a841867f6de9d9646d6106d38
Samarah Mustafá <samarah.dev@gmail.com>
2025-10-18 14:56:43 -0300

    Retornando um array com mais de n caracteres

---
+++ Exercício 94.js
@@ -0,0 +1,8 @@
+//Exercício 94: Dado um array de strings e um número n, retorne um novo array contendo apenas as strings que têm mais de n caracteres.
+function filtrarPorComprimento(array, n) {
+    return array.filter(item => item.length > n);
+}
+
+const arrayDeStrings = ["banana", "maçã", "laranja", "kiwi"];
+const resultado = filtrarPorComprimento(arrayDeStrings, 5);
+console.log(resultado);
