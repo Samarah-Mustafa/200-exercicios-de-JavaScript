1569a7d85587d554f810098673a3a1e9f950583b
Samarah Mustafá <samarah.dev@gmail.com>
2025-10-25 18:01:00 -0300

    Idade e Peso

--- Exercício 01.js
+++
@@ -1,6 +0,0 @@
-//Exercício 01: Declare duas variáveis chamadas "idade" e "peso", atribua valores a elas e imprima os valores na console.
-
-let idade = 38;
-let peso = 47;
-console.log("Sua idade é: " + idade);
-console.log("Seu peso é: " + peso);
