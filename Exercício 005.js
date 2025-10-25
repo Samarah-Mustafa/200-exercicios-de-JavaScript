4896212f0188e00ff0ffc37c9966dac9c0d0031e
Samarah Mustafá <samarah.dev@gmail.com>
2025-10-25 18:23:54 -0300

    Adição entre dois números

--- Exercício 005.js
+++ Exercício 005.js
@@ -1,7 +1,6 @@
 //Exercício 5: Declare duas variáveis e atribua valores numéricos a elas.
 // Use o operador de adição para realizar operações entre as duas variáveis.
 // Imprima o resultado.
-
 let num1 = 30;
 let num2 = 15;
 let resultado = num1 + num2;
