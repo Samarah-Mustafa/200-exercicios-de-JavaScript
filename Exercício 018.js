ad812cde050e0c1d8bdf54d7c5a50f9e3789acb2
Samarah Mustafá <samarah.dev@gmail.com>
2025-10-26 14:06:01 -0300

    Verificando Idade para dirigir

--- Exercício 018.js
+++ Exercício 018.js
@@ -1,5 +1,5 @@
 //Exercício 18: Escreva um programa que verifica se uma pessoa pode dirigir com base na idade.
-const idade = 16;
+const idade = 17;
 
 if (idade >= 18) {
     console.log("Você pode dirigir.");
