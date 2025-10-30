3b889b368a8c828123eca3eb533aaf9a78fb773c
Samarah Mustafá <samarah.dev@gmail.com>
2025-10-29 22:47:07 -0300

    Imprimindo todos os números perfeitos de 1 a 100

--- Exercício 036.js
+++ Exercício 036.js
@@ -1,4 +1,6 @@
-//Exercício 36: Usando um laço for, imprima todos os números perfeitos de 1 a 100 (um número é perfeito se a soma de seus divisores, incluindo 1 mas não ele mesmo, é igual ao próprio número).
+//Exercício 36: Usando um laço for, imprima todos os números perfeitos de 1 a 100
+// (um número é perfeito se a soma de seus divisores, incluindo 1 mas não ele mesmo,
+// é igual ao próprio número).
 for (let num = 1; num <= 100; num++) {
     let somaDivisores = 0;
     for (let i = 1; i < num; i++) {
