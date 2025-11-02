a160d127adb30c7c94ca700425b03ed4d6893895
Samarah Mustafá <samarah.dev@gmail.com>
2025-11-02 11:35:00 -0300

    Verificando se um número é um palíndromo

--- Exercício 043.js
+++ Exercício 043.js
@@ -1,4 +1,5 @@
-//Exercício 43: Usando um laço while, crie um programa que determine se um número é palíndromo ou não (um número é palíndromo se for igual ao seu reverso).
+//Exercício 43: Usando um laço while, crie um programa que determine se um número é palíndromo
+//ou não (um número é palíndromo se for igual ao seu reverso).
 let numero = 232;
 let numeroInvertido = 0;
 let numeroOriginal = numero;
