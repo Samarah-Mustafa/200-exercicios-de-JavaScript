2167514c706a98e10335bbc856ce3d92951358e2
Samarah Mustafá <samarah.dev@gmail.com>
2025-11-05 22:48:24 -0300

    Retornando um array condicionado pela função de callback

--- Exercício 068.js
+++ Exercício 068.js
@@ -1,4 +1,6 @@
-//Exercício 68: Escreva uma função de alta ordem que aceite um array de números e uma função de callback, e retorne um novo array que contém apenas os números do array original que satisfazem a condição da função de callback.
+/*Exercício 68: Escreva uma função de alta ordem que aceite um array de números e uma função de callback,
+e retorne um novo array que contém apenas os números do array original que satisfazem
+a condição da função de callback*/
 function filtrarNumeros(array, callback) {
     return array.filter(callback);
 }
