d7a80c08fc90dee2de91234cf74407fb9001b1e8
Samarah Mustafá <samarah.dev@gmail.com>
2025-11-05 22:50:14 -0300

    Executando a função de callback em um número de vezes

--- Exercício 069.js
+++ Exercício 069.js
@@ -1,4 +1,5 @@
-//Exercício 69: Escreva uma função que aceite um número e uma função de callback, e execute a função de callback o número de vezes especificado.
+/*Exercício 69: Escreva uma função que aceite um número e uma função de callback,
+e execute a função de callback o número de vezes especificado*/
 function executarCallbackVezes(vezes, callback) {
     for (let i = 0; i < vezes; i++) {
         callback(i);
