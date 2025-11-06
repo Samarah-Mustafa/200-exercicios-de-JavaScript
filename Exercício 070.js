0a6b58a6f887fa89da866f297e1aed30be5d2441
Samarah Mustafá <samarah.dev@gmail.com>
2025-11-05 22:55:55 -0300

    Retornando resultados de um array com função de callback

--- Exercício 070.js
+++ Exercício 070.js
@@ -1,4 +1,5 @@
-//Exercício 70: Escreva uma função de alta ordem que aceite um array de funções de callback e um valor, e retorne um array de resultados de aplicar cada função de callback ao valor.
+/*Exercício 70: Escreva uma função de alta ordem que aceite um array de funções de callback e um valor,
+e retorne um array de resultados de aplicar cada função de callback ao valor*/
 function aplicarCallbacks(callbacks, valor) {
     return callbacks.map(callback => callback(valor));
 }
