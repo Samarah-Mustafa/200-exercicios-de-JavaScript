4f3308bb43d5c37e35aeee58e39b20cde25bccb1
Samarah Mustafá <samarah.dev@gmail.com>
2025-11-05 22:12:00 -0300

    Função que retorne a raiz quadrada dos números de um array

--- Exercício 61.js
+++
@@ -1,5 +0,0 @@
-//Exercício 61: Escreva uma função que receba um array de números e retorne um novo array com a raiz quadrada de todos os números.
-function numerosRaizQuadrada(array) {
-    return array.map(num => Math.sqrt(num));
-}
-console.log(numerosRaizQuadrada([1, 4, 9, 16]));
