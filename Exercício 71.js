ca4bf7ba7cfbfa7ebf9529a6f087c9eaef42c225
Samarah Mustafá <samarah.dev@gmail.com>
2025-11-06 23:07:49 -0300

    Executando a função de callback assincronicamente

--- Exercício 71.js
+++
@@ -1,9 +0,0 @@
-//Exercício 71: Escreva uma função que aceite uma função de callback e um número, e execute a função de callback após um certo número de milissegundos especificados pelo número.
-function executarAposDelay(callback, delay) {
-    setTimeout(callback, delay);
-}
-let callback = () => {
-    console.log("Executando após o delay!");
-}
-
-executarAposDelay(callback, 2000);
