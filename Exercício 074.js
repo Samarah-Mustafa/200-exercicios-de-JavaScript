c98f2b85eab46b0d68f49557244ae33373924ac8
Samarah Mustafá <samarah.dev@gmail.com>
2025-11-08 09:33:52 -0300

    Criando um array novo a partir de outro

--- Exercício 074.js
+++ Exercício 074.js
@@ -1,4 +1,6 @@
-//Exercício 74: Escreva uma função de alta ordem que aceite uma função de callback e um array, e retorne um novo array que contém os resultados de aplicar a função de callback a cada elemento do array, sem alterar o array original.
+/*Exercício 74: Escreva uma função de alta ordem que aceite uma função de callback e um array,
+e retorne um novo array que contém os resultados de aplicar a função de callback
+a cada elemento do array, sem alterar o array original.*/
 function minhaFuncaoMapeamento(callback, arr) {
     const novoArray = [];
 
