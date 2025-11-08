e5cd3b45a4fab7d783b6164a168aab77d74d5dcb
Samarah Mustafá <samarah.dev@gmail.com>
2025-11-08 10:20:51 -0300

    Combinando arrays e filtrando múltiplos de 5

--- Exercício 087.js
+++ Exercício 087.js
@@ -1,4 +1,5 @@
-//Exercício 87: Dado dois arrays, um com vários números pares e outro com números ímpares, combine-os e em seguida, filtre apenas os números que são múltiplos de 5.
+/*Exercício 87: Dado dois arrays, um com vários números pares e outro com números ímpares,
+combine-os e em seguida, filtre apenas os números que são múltiplos de 5.*/
 const arrayPares = [2, 4, 10, 12, 20, 30, 8, 16];
 const arrayImpares = [1, 5, 15, 7, 25, 3, 11, 35];
 
