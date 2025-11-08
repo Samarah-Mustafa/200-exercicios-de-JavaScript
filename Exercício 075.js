9c6ba61893fa7d0174695afc8ed56961010043e3
Samarah Mustafá <samarah.dev@gmail.com>
2025-11-08 09:38:11 -0300

    Resultado de callback aplicado em ordem inversa

--- Exercício 075.js
+++ Exercício 075.js
@@ -1,4 +1,6 @@
-//Exercício 75: Escreva uma função de alta ordem que aceite um array de funções de callback e um valor, e retorne um novo array que contém os resultados de aplicar a função de callback ao valor, na ordem inversa.
+/*Exercício 75: Escreva uma função de alta ordem que aceite um array de funções de callback
+e um valor, e retorne um novo array que contém os resultados de aplicar a função de callback
+ao valor, na ordem inversa.*/
 function aplicarInverso(callbacks, valorInicial) {
     const callbacksInvertidas = [...callbacks].reverse();
 
