9412ce180bf2e7dea63f177c3b904b26de9fecf8
Samarah Mustafá <samarah.dev@gmail.com>
2025-11-08 09:41:28 -0300

    Criando um objeto vazio e adicionando propriedades

--- Exercício 76.js
+++ Exercício 76.js
@@ -1,4 +1,5 @@
-//Exercício 76: Crie um objeto vazio e adicione três propriedades a ele, cada uma com diferentes tipos de valores.
+/*Exercício 76: Crie um objeto vazio e adicione três propriedades a ele,
+cada uma com diferentes tipos de valores.*/
 const meuObjeto = {};
 
 meuObjeto.string = "Olá, mundo!";
